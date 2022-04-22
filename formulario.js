631f67d553600a00c7aefaa6eaea856908acb9c3
MollyCutbill <95192023+MollyCutbill@users.noreply.github.com>
2022-04-22 15:43:13 -0400

    commits

--- formulario.js
+++ formulario.js
@@ -30,6 +30,54 @@ $(document).ready(function() {
     });
 });
 
+
+
+
+//Validaciones JS
+
+
+//const nombre = document.getElementById("firstname")
+//const apellido = document.getElementById("lastname")
+//const email = document.getElementById("email")
+//const contra = document.getElementById("password")
+//const form = document.getElementById("form")
+//const warning = document.getElementById("warnings")
+
+//form.addEventListener("submit", e => {
+//        e.preventDefault()
+//        let warnings = ""
+//        let entrar = false
+//        let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/
+//        if (firsname.value.length < 3) {
+//            warnings += 'El nombre no es valido <br>'
+//            entrar = true
+//        }
+
+//        if (lastname.value.length < 3) {
+//            warnings += 'El apellido no es valido <br>'
+//            entrar = true
+//        }
+
+//        if (!regexEmail.test(email.value)) {
+//            warnings += 'El email no es valido <br>'
+//            entrar = true
+
+//        }
+
+//        if (contra.value.length < 8) {
+//            warnings += 'La contraseña no es valida <br>'
+//            entrar = true
+//        }
+
+//        if (entrar) {
+//            warning.innerHTML = "warnings"
+//        }
+//    })
+
+
+
+
+
 //menu
 
 //let menu2 = document.querySelectorAll(".menu2")[0];
