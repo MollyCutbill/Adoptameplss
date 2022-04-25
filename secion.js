9dc8764d1d8ac4612bd777e7c3bcad574ae55ac2
enzoMenchise <en.gomezm@duocuc.cl>
2022-04-25 13:42:08 -0400

    commits

---
+++ secion.js
@@ -0,0 +1,78 @@
+const formulario = document.getElementById('formulario');  /*acceso al id=formulario*/
+const inputs = document.querySelectorAll('#formulario input'); /*optiene todos los arreglos de los input*/
+
+const expresiones = {
+	usuario: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, /* Letras y espacios, pueden llevar acentos.*/
+    password: /^.{4,12}$/, // 4 a 12 digitos.
+    
+	
+	
+}
+
+
+const campos = {
+	usuario: false,
+    password: false,
+   
+}
+
+
+const validarFormulario = (e) => {
+	switch (e.target.name) {
+		case "usuario":
+			validarCampo(expresiones.usuario, e.target, 'usuario');
+		break;
+		case "password":
+			validarCampo(expresiones.password, e.target, 'password');
+		break;
+        
+        
+        
+	}
+}
+
+const validarCampo = (expresion, input, campo) => {
+	if(expresion.test(input.value)){
+		document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-incorrecto');
+		document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-correcto');
+		document.querySelector(`#grupo__${campo} i`).classList.add('fa-check-circle');
+		document.querySelector(`#grupo__${campo} i`).classList.remove('fa-times-circle');
+		document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.remove('formulario__input-error-activo');
+		campos[campo] = true;
+	} else {
+		document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-incorrecto');
+		document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-correcto');
+		document.querySelector(`#grupo__${campo} i`).classList.add('fa-times-circle');
+		document.querySelector(`#grupo__${campo} i`).classList.remove('fa-check-circle');
+		document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.add('formulario__input-error-activo');
+		campos[campo] = false;
+	}
+}
+
+
+
+inputs.forEach((input) => {
+	input.addEventListener('keyup', validarFormulario);
+	input.addEventListener('blur', validarFormulario);
+});
+
+formulario.addEventListener('submit', (e) => {
+	e.preventDefault();
+
+	
+
+	if(campos.usuario  && campos.password /*&& campos.fecha && campos.hora*/){
+		formulario.reset();
+
+		document.getElementById('formulario__mensaje-exito').classList.add('formulario__mensaje-exito-activo');
+		setTimeout(() => {
+			document.getElementById('formulario__mensaje-exito').classList.remove('formulario__mensaje-exito-activo');
+		}, 5000);
+
+		document.querySelectorAll('.formulario__grupo-correcto').forEach((icono) => {
+			icono.classList.remove('formulario__grupo-correcto');
+		});
+	} else {
+		document.getElementById('formulario__mensaje').classList.add('formulario__mensaje-activo');
+	}
+});
